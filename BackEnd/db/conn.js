022c68fa330937980a717be9543ea71ba6e6e8f0
Dev-Caua-Ricken <cauaricken@gmail.com>
2026-07-02 21:27:38 -0300

    ajustes de url

--- BackEnd/db/conn.js
+++ BackEnd/db/conn.js
@@ -2,15 +2,14 @@ require("dotenv").config();
 
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-  }
-);
+const sequelize = new Sequelize(process.env.MYSQL_PUBLIC_URL, {
+  dialect: "mysql",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 module.exports = sequelize;
